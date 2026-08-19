4d59958a7421ecfdd099c8844d13db7632c6f71b
Zahira Mohammed <155492447+Zahira313@users.noreply.github.com>
2026-08-19 16:33:50 +0100

    Update 01_product_price_categories.sql

--- End_to_End_Marketing_Analytics/SQL/01_product_price_categories.sql
+++ End_to_End_Marketing_Analytics/SQL/01_product_price_categories.sql
@@ -1,1 +1,19 @@
+-- Categorise products based on price
+-- Low: < 50
+-- Medium: 50–200
+-- High: > 200
 
+SELECT 
+    ProductID,
+    ProductName,
+    Price,
+    CASE
+        WHEN Price < 50 THEN 'Low'
+
+        WHEN Price BETWEEN 50 AND 200 THEN 'Medium'
+
+        ELSE 'High'
+
+    END AS PriceCategory
+
+FROM dbo.products;
